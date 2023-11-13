bb34fddd08781ba1dec2518217c77ea68ffefaf8
kizito917 <johnkingsley917@gmail.com>
2023-11-13 21:24:55 +0100

    Shadcn ui setup with javascript.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom';
+import { Button } from '../components/ui/button';
 
 const Home = () => {
     return (
         <>
             <h2>Welcome to my Home page</h2>
+            <Button variant="destructive">Click me</Button>
             <Link to="/about">Go to about page</Link>
         </>
     )
